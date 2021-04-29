d8d1c60a95f75b38937830ca4915454f10f05533
Joel <Joel@LAPTOP-4HM0VHPC>
2021-04-29 23:51:04 +0530

    Added Forms

--- Mini_Project/Program.cs
+++ Mini_Project/Program.cs
@@ -16,7 +16,7 @@ namespace Mini_Project
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Staff_Accounts());
+            Application.Run(new StartUp());
         }
     }
 }
